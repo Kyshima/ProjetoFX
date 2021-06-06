1ac93b71e299e26acbf327b6ec5484e139831d53
rpcmoreira <rpcardoso2@outlook.com>
2021-06-06 13:12:42 +0100

    nfubrgvu

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -69,6 +69,11 @@ public class Controller {
     public String input = "";
 
     public double radius = 15;
+
+    public TextField idUserField;
+    public TextField nomeUserField;
+    public TextField tipoUserField;
+
     int[] sizes = new int[8];
     protected static final int GROUP_MARGIN = 10;
     SequentialSearchST<Integer, User> user_st = new SequentialSearchST<>();
@@ -250,10 +255,10 @@ public class Controller {
         }
     }
 
-    public void handleButtonEdit(javafx.event.ActionEvent actionEvent) {
+    public void handleButtonEdit(ActionEvent actionEvent) {
     }
 
-    public void handleButtonRemove(javafx.event.ActionEvent actionEvent) {
+    public void handleButtonRemove(ActionEvent actionEvent) {
     }
 
     public void createDist(ActionEvent actionEvent) {
@@ -313,14 +318,17 @@ public class Controller {
         itemTable.setItems(itemOL());
 
         addAndEditUser();
+        addAndEditReg();
+        addAndEditGeo();
+        addAndEditItens();
     }
 
     public void addAndEditUser(){
         userTable.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
 
             if( event.getCode() == KeyCode.ENTER) {
-                System.out.println(input);
-                editPos();
+                //System.out.println(input);
+                editPosUser();
                 input = "";
                 return;
             }
@@ -340,7 +348,7 @@ public class Controller {
                 if (pos.getRow() == -1) {
                     userTable.getSelectionModel().select(0);
                 } else if (pos.getRow() == userTable.getItems().size() -1) {
-                    addRow();
+                    addRowUser();
                 } else if (pos.getRow() < userTable.getItems().size() -1) {
                     userTable.getSelectionModel().clearAndSelect( pos.getRow() + 1, pos.getTableColumn());
                 }
@@ -351,7 +359,7 @@ public class Controller {
                 if (pos.getRow() == -1) {
                     userTable.getSelectionModel().select(0);
                 } else {
-                    removeSelectedRows();
+                    removeSelectedRowsUser();
                 }
             }
         });
@@ -361,7 +369,7 @@ public class Controller {
         userTable.getSelectionModel().selectFirst();
     }
 
-    public void addRow() {
+    public void addRowUser() {
         TablePosition pos = userTable.getFocusModel().getFocusedCell();
         userTable.getSelectionModel().clearSelection();
         User user = new User(0,"0","0");
@@ -371,7 +379,7 @@ public class Controller {
         userTable.scrollTo(user);
     }
 
-    public void editPos() {
+    public void editPosUser() {
         TablePosition pos = userTable.getFocusModel().getFocusedCell();
         int r = pos.getRow();
         User user;
@@ -386,13 +394,265 @@ public class Controller {
         }
         userTable.getItems().add(user);
         userTable.getSelectionModel().select( r, pos.getTableColumn());
-        removeSelectedRows();
+        removeSelectedRowsUser();
         userTable.scrollTo(user);
     }
 
-    public void removeSelectedRows() {
+    public void removeSelectedRowsUser() {
         userTable.getItems().removeAll(userTable.getSelectionModel().getSelectedItems());
         userTable.getSelectionModel().clearSelection();
+    }
+
+    public void addAndEditReg(){
+        regTable.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+
+            if( event.getCode() == KeyCode.ENTER) {
+                //System.out.println(input);
+                editPosReg();
+                input = "";
+                return;
+            }
+
+            if(regTable.getEditingCell() == null) {
+                if(event.getCode().isLetterKey() || event.getCode().isDigitKey()) {
+                    input = input + event.getText();
+                }
+            }
+        });
+
+        regTable.addEventFilter(KeyEvent.KEY_RELEASED, event -> {
+
+            if(event.getCode() == KeyCode.ENTER && input.equals("")) {
+                TablePosition pos = regTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    regTable.getSelectionModel().select(0);
+                } else if (pos.getRow() == regTable.getItems().size() -1) {
+                    addRowReg();
+                } else if (pos.getRow() < regTable.getItems().size() -1) {
+                    regTable.getSelectionModel().clearAndSelect( pos.getRow() + 1, pos.getTableColumn());
+                }
+            }
+            if(event.getCode() == KeyCode.DELETE){
+                TablePosition pos = userTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    userTable.getSelectionModel().select(0);
+                } else {
+                    removeSelectedRowsReg();
+                }
+            }
+        });
+
+        regTable.getSelectionModel().setCellSelectionEnabled(true);
+        regTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        regTable.getSelectionModel().selectFirst();
+    }
+
+    public void addRowReg() {
+        TablePosition pos = regTable.getFocusModel().getFocusedCell();
+        regTable.getSelectionModel().clearSelection();
+        Regiao reg = new Regiao(0,"0",0);
+        regTable.getItems().add(reg);
+        int row = regTable.getItems().size() - 1;
+        regTable.getSelectionModel().select( row, pos.getTableColumn());
+        regTable.scrollTo(reg);
+    }
+
+    public void editPosReg() {
+        TablePosition pos = regTable.getFocusModel().getFocusedCell();
+        int r = pos.getRow();
+        Regiao reg;
+        switch(pos.getColumn()){
+            case 0: reg = new Regiao(Integer.parseInt(input),reg_st.get(r+1).nome,reg_st.get(r+1).n_caches);
+                break;
+            case 1: reg = new Regiao(reg_st.get(r+1).id,input,reg_st.get(r+1).n_caches);
+                break;
+            case 2: reg = new Regiao(reg_st.get(r+1).id,reg_st.get(r+1).nome,Integer.parseInt(input));
+                break;
+            default: reg = new Regiao(0,"0",0);
+        }
+        regTable.getItems().add(reg);
+        regTable.getSelectionModel().select( r, pos.getTableColumn());
+        removeSelectedRowsReg();
+        regTable.scrollTo(reg);
+    }
+
+    public void removeSelectedRowsReg() {
+        regTable.getItems().removeAll(regTable.getSelectionModel().getSelectedItems());
+        regTable.getSelectionModel().clearSelection();
+    }
+
+    public void addAndEditGeo(){
+        geoTable.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+
+            if( event.getCode() == KeyCode.ENTER) {
+                //System.out.println(input);
+                editPosGeo();
+                input = "";
+                return;
+            }
+
+            if(geoTable.getEditingCell() == null) {
+                if(event.getCode().isLetterKey() || event.getCode().isDigitKey()) {
+                    input = input + event.getText();
+                }
+            }
+        });
+
+        geoTable.addEventFilter(KeyEvent.KEY_RELEASED, event -> {
+
+            if(event.getCode() == KeyCode.ENTER && input.equals("")) {
+                TablePosition pos = geoTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    geoTable.getSelectionModel().select(0);
+                } else if (pos.getRow() == geoTable.getItems().size() -1) {
+                    addRowGeo();
+                } else if (pos.getRow() < geoTable.getItems().size() -1) {
+                    geoTable.getSelectionModel().clearAndSelect( pos.getRow() + 1, pos.getTableColumn());
+                }
+            }
+            if(event.getCode() == KeyCode.DELETE){
+                TablePosition pos = geoTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    geoTable.getSelectionModel().select(0);
+                } else {
+                    removeSelectedRowsGeo();
+                }
+            }
+        });
+
+        geoTable.getSelectionModel().setCellSelectionEnabled(true);
+        geoTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        geoTable.getSelectionModel().selectFirst();
+    }
+
+    public void addRowGeo() {
+        TablePosition pos = geoTable.getFocusModel().getFocusedCell();
+        geoTable.getSelectionModel().clearSelection();
+        Geocache geocache = new Geocache("geocache", "0", 0.0f, 0.0f, 0,0);
+        geoTable.getItems().add(geocache);
+        int row = geoTable.getItems().size() - 1;
+        geoTable.getSelectionModel().select( row, pos.getTableColumn());
+        geoTable.scrollTo(geocache);
+    }
+
+    public void editPosGeo() {
+        TablePosition pos = geoTable.getFocusModel().getFocusedCell();
+        int r = pos.getRow();
+        Geocache geo;
+        switch(pos.getColumn()){
+            case 0: geo = new Geocache("geocache"+Integer.parseInt(input),geo_st.get(r+1).tipo,
+                    geo_st.get(r+1).coordenadasX,geo_st.get(r+1).coordenadasY,geo_st.get(r+1).id_reg, geo_st.get(r+1).n_itens);
+                break;
+            case 1: geo = new Geocache(geo_st.get(r+1).id,input,
+                    geo_st.get(r+1).coordenadasX,geo_st.get(r+1).coordenadasY,geo_st.get(r+1).id_reg, geo_st.get(r+1).n_itens);
+                break;
+            case 2: geo = new Geocache(geo_st.get(r+1).id,geo_st.get(r+1).tipo,
+                    Float.parseFloat(input),geo_st.get(r+1).coordenadasY,geo_st.get(r+1).id_reg, geo_st.get(r+1).n_itens);
+                break;
+            case 3: geo = new Geocache(geo_st.get(r+1).id,geo_st.get(r+1).tipo,
+                    geo_st.get(r+1).coordenadasX,Float.parseFloat(input),geo_st.get(r+1).id_reg, geo_st.get(r+1).n_itens);
+                break;
+            case 4: geo = new Geocache(geo_st.get(r+1).id,geo_st.get(r+1).tipo,
+                    geo_st.get(r+1).coordenadasX,geo_st.get(r+1).coordenadasY,Integer.parseInt(input), geo_st.get(r+1).n_itens);
+                break;
+            case 5: geo = new Geocache(geo_st.get(r+1).id,geo_st.get(r+1).tipo,
+                    geo_st.get(r+1).coordenadasX,geo_st.get(r+1).coordenadasY,geo_st.get(r+1).id_reg, Integer.parseInt(input));
+                break;
+            default: geo = new Geocache("geocache0", "0", 0.0f, 0.0f, 0,0);
+        }
+        geoTable.getItems().add(geo);
+        geoTable.getSelectionModel().select( r, pos.getTableColumn());
+        removeSelectedRowsReg();
+        geoTable.scrollTo(geo);
+    }
+
+    public void removeSelectedRowsGeo() {
+        geoTable.getItems().removeAll(geoTable.getSelectionModel().getSelectedItems());
+        geoTable.getSelectionModel().clearSelection();
+    }
+
+    public void addAndEditItens(){
+        itemTable.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+
+            if( event.getCode() == KeyCode.ENTER) {
+                //System.out.println(input);
+                editPosItens();
+                input = "";
+                return;
+            }
+
+            if(itemTable.getEditingCell() == null) {
+                if(event.getCode().isLetterKey() || event.getCode().isDigitKey()) {
+                    input = input + event.getText();
+                }
+            }
+        });
+
+        itemTable.addEventFilter(KeyEvent.KEY_RELEASED, event -> {
+
+            if(event.getCode() == KeyCode.ENTER && input.equals("")) {
+                TablePosition pos = itemTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    itemTable.getSelectionModel().select(0);
+                } else if (pos.getRow() == itemTable.getItems().size() -1) {
+                    addRowItens();
+                } else if (pos.getRow() < itemTable.getItems().size() -1) {
+                    itemTable.getSelectionModel().clearAndSelect( pos.getRow() + 1, pos.getTableColumn());
+                }
+            }
+            if(event.getCode() == KeyCode.DELETE){
+                TablePosition pos = itemTable.getFocusModel().getFocusedCell();
+
+                if (pos.getRow() == -1) {
+                    itemTable.getSelectionModel().select(0);
+                } else {
+                    removeSelectedRowsItens();
+                }
+            }
+        });
+
+        itemTable.getSelectionModel().setCellSelectionEnabled(true);
+        itemTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        itemTable.getSelectionModel().selectFirst();
+    }
+
+    public void addRowItens() {
+        TablePosition pos = itemTable.getFocusModel().getFocusedCell();
+        itemTable.getSelectionModel().clearSelection();
+        Item item = new Item(0,"geocache0","");
+        itemTable.getItems().add(item);
+        int row = itemTable.getItems().size() - 1;
+        itemTable.getSelectionModel().select( row, pos.getTableColumn());
+        itemTable.scrollTo(item);
+    }
+
+    public void editPosItens() {
+        TablePosition pos = itemTable.getFocusModel().getFocusedCell();
+        int r = pos.getRow();
+        Item item;
+        switch(pos.getColumn()){
+            case 0: item = new Item(Integer.parseInt(input),item_st.get(r+1).id_geo,item_st.get(r+1).item);
+                break;
+            case 1: item = new Item(item_st.get(r+1).id,"geocache"+Integer.parseInt(input),item_st.get(r+1).item);
+                break;
+            case 2: item = new Item(item_st.get(r+1).id,item_st.get(r+1).id_geo,input);
+                break;
+            default: item = new Item(0,"geocache0","");
+        }
+        itemTable.getItems().add(item);
+        itemTable.getSelectionModel().select( r, pos.getTableColumn());
+        removeSelectedRowsItens();
+        itemTable.scrollTo(item);
+    }
+
+    public void removeSelectedRowsItens() {
+        itemTable.getItems().removeAll(itemTable.getSelectionModel().getSelectedItems());
+        itemTable.getSelectionModel().clearSelection();
     }
 
     public void lerFile(){
